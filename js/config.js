bc54c5bc74cf90dde6874a48429379623f93f7f1
kentung52 <ken4506@gmail.com>
2025-11-18 22:14:53 +0800

    news new venue

--- js/config.js
+++ js/config.js
@@ -2,7 +2,7 @@
   window.SITE = {
     PHONE_TEXT: "0966570250",                  // 顯示文字
     PHONE_URI:  "tel:0966570250",              // 連結
-    ADDRESS:    "桃園市八德區豐德二路35號",
+    ADDRESS:    "桃園市八德區東勇北路520號",
     MAP_URL:    "https://maps.app.goo.gl/zqQqQ7DXSarYD7QE6",
     WEEKDAY_HR: "平日09:00〜23:00",
     WEEKEND_HR: "假日09:00〜23:00",
