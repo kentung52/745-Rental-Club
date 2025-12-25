dd2341daa54a8725f184a8e48ec60747d40ef1eb
kentung52 <ken4506@gmail.com>
2025-12-26 01:47:10 +0800

    phone modify

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 
   window.SITE = {
-    PHONE_TEXT: "0966570250",                  // 顯示文字
-    PHONE_URI:  "tel:0966570250",              // 連結
+    PHONE_TEXT: "0909170679",                  // 顯示文字
+    PHONE_URI:  "tel:0909170679",              // 連結
     ADDRESS:    "桃園市八德區東勇北路520號",
     MAP_URL:    "https://maps.app.goo.gl/zqQqQ7DXSarYD7QE6",
     WEEKDAY_HR: "平日09:00〜23:00",
